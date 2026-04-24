40ee458aae47a21c6bc0171bc95a5d5d38e33626
Yunu Choe <yunuchoe10@gmail.com>
2026-04-24 16:26:14 -0700

    Update sobel.vhd

--- sobel.vhd
+++ sobel.vhd
@@ -1,5 +1,3 @@
-
-
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 use ieee.std_logic_unsigned.all;
@@ -29,20 +27,17 @@ type sobel_window_3_by_3_type is array (0 to 2, 0 to 2 ) of std_logic_vector(3 d
 signal sobel_line_buffer : sobel_line_buffer_type;
 signal sobel_window_3_by_3 : sobel_window_3_by_3_type;
 
-signal temp_y_1,temp_y_2,temp_x_1,temp_x_2: std_logic_vector(5 downto 0);
+signal temp_y_1: std_logic_vector(5 downto 0);
+signal temp_y_2: std_logic_vector(5 downto 0);
+signal temp_x_1: std_logic_vector(5 downto 0);
+signal temp_x_2: std_logic_vector(5 downto 0);
 
-signal datax,datay : std_logic_vector(6 downto 0);
-
+signal datax : std_logic_vector(6 downto 0);
+signal datay : std_logic_vector(6 downto 0);
 signal dataxy : std_logic_vector(7 downto 0);
 
 
-
-
-
-
-
 begin
-
 do_sobel_shift : process( 
     video_clock,
     v_sync,
@@ -51,6 +46,7 @@ do_sobel_shift : process(
     gray_pixel_in( 3 downto 0 )
 )
 variable position : integer := 0;
+
 
 begin
     if ( rising_edge( video_clock )) then
@@ -82,41 +78,27 @@ sobel_window_3_by_3( 2, 0 ) <= sobel_line_buffer( 0 + 1280 );
 sobel_window_3_by_3( 2, 1 ) <= sobel_line_buffer( 1 + 1280 );
 sobel_window_3_by_3( 2, 2 ) <= sobel_line_buffer( 2 + 1280 );
 
-        
-        
--- 
+
 -- y direction
---
+temp_y_1 <= ( '0' & sobel_window_3_by_3(0,0) & '0') + ( sobel_window_3_by_3(0,1) & "00") + ( '0' & sobel_window_3_by_3(0,2) & '0'); 
+temp_y_2 <= ( '0' & sobel_window_3_by_3(2,0) & '0') + ( sobel_window_3_by_3(2,1) & "00") + ( '0' & sobel_window_3_by_3(2,2) & '0'); 
 
-temp_y_1 <= ( '0' & sobel_window_3_by_3(0,0) & '0') + ( sobel_window_3_by_3(0,1) & "00") + ( '0' & sobel_window_3_by_3(0,2) & '0'); -- 2*win(0,0) + 4*win(0,1) + 2*win(0,2)
+datay <= (('0' & temp_y_2) - ( '0' & temp_y_1)) when ( temp_y_2 > temp_y_1) else (( '0' & temp_y_1) - ( '0' & temp_y_2));
 
-temp_y_2 <= ( '0' & sobel_window_3_by_3(2,0) & '0') + ( sobel_window_3_by_3(2,1) & "00") + ( '0' & sobel_window_3_by_3(2,2) & '0'); -- 2*win(2,0) + 4*win(2,1) + 2*win(2,2)
-datay <= (('0' & temp_y_2) - ( '0' & temp_y_1)) when ( temp_y_2 > temp_y_1) else (( '0' & temp_y_1) - ( '0' & temp_y_2)); -- abs((winIn(0,0) + 2*winIn(0,1)+ win(0,2))-((winIn(2,0) + 2*winIn(2,1)+ win(2,2)))
-
---
 --  x direction
---
-temp_x_1 <= ( '0' & sobel_window_3_by_3(0,0) & '0') + ( sobel_window_3_by_3(1,0) & "00") + ( '0' & sobel_window_3_by_3(2,0) & '0'); -- 2*win(0,0) + 4*win(1,0) + 2*win(2,0)
+temp_x_1 <= ( '0' & sobel_window_3_by_3(0,0) & '0') + ( sobel_window_3_by_3(1,0) & "00") + ( '0' & sobel_window_3_by_3(2,0) & '0');
+temp_x_2 <= ( '0' & sobel_window_3_by_3(0,2) & '0') + ( sobel_window_3_by_3(1,2) & "00") + ( '0' & sobel_window_3_by_3(2,2) & '0');
 
-temp_x_2 <= ( '0' & sobel_window_3_by_3(0,2) & '0') + ( sobel_window_3_by_3(1,2) & "00") + ( '0' & sobel_window_3_by_3(2,2) & '0'); -- 2*win(0,2) + 4*win(1,2) + 2*win(2,2)
-datax <= (( '0' & temp_x_2) - ( '0' & temp_x_1)) when ( temp_x_2 > temp_x_1) else (( '0' & temp_x_1) - ( '0' & temp_x_2)); -- abs(winIn(0,0) + 2*winIn(1,0)+ win(2,0))-(winIn(0,2) + 2*winIn(1,2)+ win(2,2)))
---
+datax <= (( '0' & temp_x_2) - ( '0' & temp_x_1)) when ( temp_x_2 > temp_x_1) else (( '0' & temp_x_1) - ( '0' & temp_x_2));
+    
 -- x and y
---
 dataxy <= ( '0' & datay ) + ( '0' & datax );
 
 
---pixel_out <= dataxy(3 downto 0) & dataxy(3 downto 0) & dataxy(3 downto 0);
---pixel_out <= dataxy(7 downto 4) & dataxy(7 downto 4) & dataxy(7 downto 4);
------------------ filter out
-
-pixel_out <=
+pixel_out <= -- update
         datax(6 downto 3) & datax(6 downto 3) & datax(6 downto 3)  when filter_options ="10" else
         datay(6 downto 3) & datay(6 downto 3) & datay(6 downto 3)  when filter_options ="01" else
         dataxy(7 downto 4) & dataxy(7 downto 4) & dataxy(7 downto 4)  when filter_options ="11" else
         dataxy(3 downto 0) & dataxy(3 downto 0) & dataxy(3 downto 0);
-
-
-
 
 end Behavioral;
